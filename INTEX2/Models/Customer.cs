a3031591ac88a81c65278e873772a834884125aa
kinseenyland <kinseenyland@gmail.com>
2024-04-09 00:00:34 -0600

    created models, some random cart stuff

---
+++ INTEX2/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace INTEX2.Models
+{
+    public class Customer
+    {
+        public int customer_ID { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set;}
+
+        //may want to change data type
+        public DateFormat birth_date { get; set; }
+        public string country_of_residence { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+    }
+}
